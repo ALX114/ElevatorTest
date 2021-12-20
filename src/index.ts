4a0bd79505c176c5baaa42f2714b19b54ab35595
ALX114 <58948125+ALX114@users.noreply.github.com>
2021-12-20 16:35:52 +0500

    Basic logic done

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import * as PIXI from "pixi.js";
 import { width, height } from "./constant";
 
+const delay = async (ms) =>
+  await new Promise((resolve) => setTimeout(resolve, ms));
+
 class ButtonOnFloor {
   levelButton: object = {};
-  private maxFloor: number = 0;
+  maxFloor: number = 0;
+  constructor() {}
   elevatorCall(floor: number) {
     this.levelButton[floor] = true;
-    console.log(this.levelButton);
   }
-  elevatorInvert(floor: number) {
-    this.levelButton[floor] = false;
+  elevatorOff(floor: number) {
+    delete this.levelButton[floor];
   }
   maxFloorCounter() {
     this.maxFloor++;
@@ -17,30 +20,18 @@ class ButtonOnFloor {
 }
 
 class Elevator {
-  private buttons = new ButtonOnFloor();
-
   elevatorMove = false;
-  buttonOnFloors: object;
-  elevatorFloor: number = 0;
-  constructor(buttons: ButtonOnFloor) {
-    this.buttonOnFloors = buttons.levelButton;
+
+  elevatorFloor: number;
+  constructor(maxFloors: number) {
+    this.elevatorFloor = maxFloors;
   }
   moveElevatorDown() {
     this.elevatorFloor--;
-    if (this.buttonOnFloors[this.elevatorFloor]) {
-      this.buttons.elevatorInvert(this.elevatorFloor);
-      delete this.buttonOnFloors[this.elevatorFloor];
-    }
-    console.log(this.elevatorFloor);
   }
 
   moveElevatorUp() {
     this.elevatorFloor++;
-    if (this.buttonOnFloors[this.elevatorFloor]) {
-      this.buttons.elevatorInvert(this.elevatorFloor);
-      delete this.buttonOnFloors[this.elevatorFloor];
-    }
-    console.log(this.elevatorFloor);
   }
 }
 
@@ -48,7 +39,6 @@ class GameRender {
   public pixiApp: PIXI.Application;
   public stage: PIXI.Container;
   private elevator: PIXI.Container;
-  private buttons: ButtonOnFloor = new ButtonOnFloor();
   constructor() {}
   initPixiApp() {
     const pixiApp = new PIXI.Application({
@@ -70,11 +60,38 @@ class GameRender {
     this.elevator.x = x;
     this.elevator.y = y;
   }
-  moveOneFloorUp() {
-    this.elevator.y -= 105;
+  async moveOneFloorUp() {
+    // console.log("ydown", this.elevator.pivot.y);
+    // let y2 = this.elevator.pivot.y + 105;
+    // let clr = setInterval(() => {
+    //   console.log("y1down", this.elevator.pivot.y, clr);
+    //   this.elevator.pivot.y++;
+    //   if (this.elevator.pivot.y >= y2) {
+    //     clearInterval(clr);
+    //   }
+    // }, 30);
+    // setTimeout(() => {
+    //   console.log(200);
+    // }, 3000);
+    for (let y = 0; y < 105; y++)
+      await delay(30).then(() => {
+        this.elevator.y--;
+      });
   }
-  moveOneFloorDown() {
-    this.elevator.y += 105;
+  async moveOneFloorDown() {
+    // console.log("ydown", this.elevator.pivot.y);
+    // let y2 = this.elevator.pivot.y - 105;
+    // let clr = setInterval(() => {
+    //   console.log("y1down", this.elevator.pivot.y, clr);
+    //   this.elevator.pivot.y--;
+    //   if (this.elevator.pivot.y <= y2) {
+    //     clearInterval(clr);
+    //   }
+    // }, 30);
+    for (let y = 0; y < 105; y++)
+      await delay(30).then(() => {
+        this.elevator.y++;
+      });
   }
   drawLevels(x: number, y: number, stage: PIXI.Container) {
     const graphics = new PIXI.Graphics();
@@ -119,54 +136,103 @@ class App {
   stage: PIXI.Container;
   elevator: PIXI.Container;
   buttons: ButtonOnFloor = new ButtonOnFloor();
-  elevatorObj = new Elevator(this.buttons);
+  elevatorObj: Elevator;
 
-  btnFloorPressed(floor: number) {
+  async btnFloorPressed(floor: number) {
+    let buttonLogic = this.buttons;
     let render = this.gameRender;
     let elevatorLogic = this.elevatorObj;
-    let dir: boolean = true;
+    let dir: boolean = false;
+
     this.buttons.elevatorCall(floor);
-    let floors = Object.keys(elevatorLogic.buttonOnFloors);
-    console.log(floors);
-    function elevatorMoveUp() {
-      elevatorLogic.moveElevatorUp();
-      render.moveOneFloorDown();
+
+    let floors: Array<string> = Object.keys(this.buttons.levelButton);
+    // console.log(floor, this.buttons.maxFloor, this.elevatorObj.elevatorFloor); //для дебага
+    // return;
+    async function elevatorMoveDown() {
+      floors = Object.keys(buttonLogic.levelButton);
+
+      while (elevatorLogic.elevatorFloor > 1) {
+        console.log("down", elevatorLogic.elevatorFloor);
+        if (buttonLogic.levelButton[elevatorLogic.elevatorFloor])
+          await delay(500);
+        buttonLogic.elevatorOff(elevatorLogic.elevatorFloor);
+
+        await render.moveOneFloorDown();
+        elevatorLogic.moveElevatorDown();
+      }
+      dir = true;
     }
-    function elevatorMoveDown() {
-      elevatorLogic.moveElevatorDown();
-      render.moveOneFloorUp();
+
+    async function elevatorMoveUp() {
+      floors = Object.keys(buttonLogic.levelButton);
+      let maxFloor = Math.max.apply(null, floors);
+      while (elevatorLogic.elevatorFloor < maxFloor) {
+        floors = Object.keys(buttonLogic.levelButton);
+        console.log("up", elevatorLogic.elevatorFloor);
+
+        await render.moveOneFloorUp();
+        elevatorLogic.moveElevatorUp();
+      }
+      dir = false;
     }
+    function elevatorInit() {
+      floors = Object.keys(buttonLogic.levelButton);
+      let maxFloor = Math.max.apply(null, floors);
+      console.log(maxFloor, elevatorLogic.elevatorFloor);
+      if (elevatorLogic.elevatorFloor < maxFloor) {
+        dir = true;
+      } else dir = false;
+    }
+    function directionLogic() {
+      let maxFloor = Math.min.apply(null, floors);
+      // if (
+      //   elevatorLogic.elevatorFloor === buttonLogic.maxFloor ||
+      //   elevatorLogic.elevatorFloor === 0
+      // ) {
+      //   dir = !dir;
+      // }
+      if (elevatorLogic.elevatorFloor > buttonLogic.maxFloor) {
+        dir = true;
+      }
+      if (elevatorLogic.elevatorFloor < 1) {
+        dir = false;
+      }
+    }
+
+    console.log("elevFloors", this.buttons.levelButton);
     if (!this.elevatorObj.elevatorMove) {
       this.elevatorObj.elevatorMove = true;
 
+      console.log("init", "start");
+      elevatorInit();
       while (floors.length !== 0) {
-        console.log("init");
-        floors = Object.keys(elevatorLogic.buttonOnFloors);
-        dir ? elevatorMoveUp() : elevatorMoveDown();
-
-        if (elevatorLogic.elevatorFloor === 0) {
-          dir = true;
-        } else if (elevatorLogic.elevatorFloor === 5) {
-          dir = false;
-        }
-        floors = Object.keys(elevatorLogic.buttonOnFloors);
+        directionLogic();
+        dir ? await elevatorMoveUp() : await elevatorMoveDown();
       }
       this.elevatorObj.elevatorMove = false;
+      console.log("init", "stop");
     }
   }
 
   constructor() {
     this.pixiApp = this.gameRender.initPixiApp();
     this.stage = this.pixiApp.stage;
-    this.gameRender.drawElevator(this.stage, 0, 5);
-
-    for (let i = 0; i < 5; i++) {
+    this.gameRender.drawElevator(this.stage, 0, 110);
+    let maxFloors = 5;
+    for (let i = maxFloors; i > 0; i--) {
       let x = 100;
       let y = i * 105 + 2;
-      let btnFunc: () => void = () => this.btnFloorPressed(i);
+      let btnFunc: () => void = () => this.btnFloorPressed(maxFloors - i + 1);
       this.gameRender.drawLevels(x, y, this.stage);
+      if (i === maxFloors) {
+        this.gameRender.drawLevels(0, y + 105, this.stage);
+      }
       this.gameRender.drawButon(x, y, this.stage, btnFunc);
+      this.buttons.maxFloorCounter();
     }
+    console.log("max", this.buttons.maxFloor);
+    this.elevatorObj = new Elevator(this.buttons.maxFloor);
   }
 }
 
